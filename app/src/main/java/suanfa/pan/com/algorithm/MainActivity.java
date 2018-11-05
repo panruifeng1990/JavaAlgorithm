847a74eed7ba3adee9e52a6d991c4c3e87906ac6
ruifeng_pan <panruifeng1990@163.com>
2018-11-05 17:58:47 +0800

    Initial commit

---
+++ app/src/main/java/suanfa/pan/com/algorithm/MainActivity.java
@@ -0,0 +1,13 @@
+package suanfa.pan.com.algorithm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
